920f7589ebb7e8d096230cbcf9d2184caafdb19b
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-03-21 14:17:04 +0300

    Add project files.

---
+++ EstateRegister/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EstateRegister.Models.Entity;
+
+namespace EstateRegister.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DistinctMvcDbEntities db=new DistinctMvcDbEntities();   
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]   
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUser(TBLKISILER p1)
+        {
+            db.TBLKISILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
